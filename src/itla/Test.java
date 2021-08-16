49354e9d270d0434d56b17c7b16da4562e90c347
Francisco Javier Medina Matos <f-javier21@hotmail.com>
2021-08-16 15:09:03 -0400

    x

---
+++ src/itla/Test.java
@@ -0,0 +1,12 @@
+
+package itla;
+
+public class Test {
+
+    public static void main(String[] args) {
+        EventosManagement em = new EventosManagement();
+        
+        em.createEvento("Test1.2","Testing","pachi","2021-08-17","12:00","12:05");
+
+    }
+}
