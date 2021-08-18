852653f0b9c7801b801b4cc6de1f42252f27b4cf
Francisco Javier Medina Matos <f-javier21@hotmail.com>
2021-08-17 23:00:42 -0400

    intancia privada en la clase ahora son usadas como localVar en los metodos

--- src/itla/DBManager.java
+++ src/itla/DBManager.java
@@ -4,16 +4,11 @@ import java.sql.*;
 
 class DBManager {
 
-    private Connection conn;
-    private ResultSet rs = null;
-    private PreparedStatement pstmnt = null;
-    private Statement stmnt = null;
-    private String sql = "";
-
     //private String url = "jdbc:mysql://localhost/agendaeventos?"; //Laptop
     private String url = "jdbc:mysql://localhost:3307/agendaeventos?"; //Desktop
     private String user = "root";
     private String password = "";
+    private Connection conn;
 
     void connectDB() {
         try {
@@ -34,21 +29,22 @@ class DBManager {
     }
 
     void insertRegistro(Evento e) {
-        this.sql = "INSERT INTO Eventos (Nombre, Detalle, Lugar, Fecha, HoraInicio, HoraFin) VALUES (?,?,?,?,?,?)";
+        PreparedStatement pstmnt;
+        String sql = "INSERT INTO Eventos (Nombre, Detalle, Lugar, Fecha, HoraInicio, HoraFin) VALUES (?,?,?,?,?,?)";
 
         try {
             this.connectDB();
-            this.pstmnt = this.conn.prepareStatement(this.sql);
+            pstmnt = this.conn.prepareStatement(sql);
 
-            this.pstmnt.setString(1, e.getNombre());
-            this.pstmnt.setString(2, e.getDetalle());
-            this.pstmnt.setString(3, e.getLugar());
-            this.pstmnt.setDate(4, e.getFecha());
-            this.pstmnt.setTime(5, e.getHoraInicio());
-            this.pstmnt.setTime(6, e.getHoraFin());
-            this.pstmnt.executeUpdate();
-
-            this.pstmnt.close();
+            pstmnt.setString(1, e.getNombre());
+            pstmnt.setString(2, e.getDetalle());
+            pstmnt.setString(3, e.getLugar());
+            pstmnt.setDate(4, e.getFecha());
+            pstmnt.setTime(5, e.getHoraInicio());
+            pstmnt.setTime(6, e.getHoraFin());
+            pstmnt.executeUpdate();
+            
+            pstmnt.close();
             System.out.println("Registro agregado");
             this.disconnectDB();
         } catch (SQLException ex) {
@@ -56,10 +52,12 @@ class DBManager {
         }
     }
 
-    @SuppressWarnings("empty-statement")
     void updateRegistro(int id, Evento e) {
-        this.sql = "UPDATE Eventos SET Nombre = ?, Detalle = ?, Lugar = ?, Fecha = ?, HoraInicio = ?, HoraFin = ? WHERE IdEvento = " + id;
+        PreparedStatement pstmnt;
+        Statement stmnt;
+        ResultSet rs;
 
+        String updateSQL = "UPDATE Eventos SET Nombre = ?, Detalle = ?, Lugar = ?, Fecha = ?, HoraInicio = ?, HoraFin = ? WHERE IdEvento = " + id;
         String selectSQL = "SELECT Nombre, Detalle, Lugar, Fecha, HoraInicio, HoraFin FROM Eventos WHERE IdEvento = " + id;
         
         String n = "";
@@ -71,17 +69,17 @@ class DBManager {
 
         try {
             this.connectDB();
-            this.stmnt = this.conn.createStatement();
-            this.pstmnt = this.conn.prepareStatement(this.sql);
+            stmnt = this.conn.createStatement();
+            pstmnt = this.conn.prepareStatement(updateSQL);
 
-            this.rs = this.stmnt.executeQuery(selectSQL);
-            if (this.rs.next()) {
-                n = this.rs.getString("Nombre");
-                d = this.rs.getString("Detalle");
-                l = this.rs.getString("Lugar");
-                f = this.rs.getDate("Fecha");
-                hI = this.rs.getTime("HoraInicio");
-                hF = this.rs.getTime("HoraFin");
+            rs = stmnt.executeQuery(selectSQL);
+            if (rs.next()) {
+                n = rs.getString("Nombre");
+                d = rs.getString("Detalle");
+                l = rs.getString("Lugar");
+                f = rs.getDate("Fecha");
+                hI = rs.getTime("HoraInicio");
+                hF = rs.getTime("HoraFin");
             }
 
             n = (e.getNombre() == null) ? n : e.getNombre();
@@ -91,16 +89,16 @@ class DBManager {
             hI = (e.getHoraInicio() == null) ? hI : e.getHoraInicio();
             hF = (e.getHoraFin() == null) ? hF : e.getHoraFin();
 
-            this.pstmnt.setString(1, n);
-            this.pstmnt.setString(2, d);
-            this.pstmnt.setString(3, l);
-            this.pstmnt.setDate(4, f);
-            this.pstmnt.setTime(5, hI);
-            this.pstmnt.setTime(6, hF);
-            this.pstmnt.executeUpdate();
-
-            this.stmnt.close();
-            this.pstmnt.close();
+            pstmnt.setString(1, n);
+            pstmnt.setString(2, d);
+            pstmnt.setString(3, l);
+            pstmnt.setDate(4, f);
+            pstmnt.setTime(5, hI);
+            pstmnt.setTime(6, hF);
+            pstmnt.executeUpdate();
+            
+            stmnt.close();
+            pstmnt.close();
             System.out.println("Registro actualizado");
             this.disconnectDB();
         } catch (SQLException ex) {
@@ -109,13 +107,16 @@ class DBManager {
     }
 
     void deleteRegistro(int id) {
-        this.sql = "DELETE FROM Eventos WHERE IdEvento = " + id;
+        Statement stmnt;
+        String deleteSQL = "DELETE FROM Eventos WHERE IdEvento = " + id;
 
         try {
             this.connectDB();
-            this.stmnt = this.conn.createStatement();
-            this.stmnt.executeUpdate(this.sql);
-            this.stmnt.close();
+            stmnt = this.conn.createStatement();
+            
+            stmnt.executeUpdate(deleteSQL);
+            
+            stmnt.close();
             System.out.println("Registro eliminado");
             this.disconnectDB();
         } catch (SQLException ex) {
