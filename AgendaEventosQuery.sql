4307e6f7501a9b06af80850e2d15c070516e66c8
Francisco Javier Medina Matos <f-javier21@hotmail.com>
2021-08-19 09:47:26 -0400

    ddd

---
+++ AgendaEventosQuery.sql
@@ -0,0 +1,17 @@
+-- CREATE DATABASE AgendaEventos;
+-- USE AgendaEventos;
+
+-- CREATE TABLE IF NOT EXISTS Eventos(
+--                                     IdEvento INT NOT NULL AUTO_INCREMENT,
+--                                      Nombre VARCHAR(40),
+--                                      Detalle TEXT,
+--                                      Lugar VARCHAR(50),
+--                                      Fecha DATE,
+--                                      HoraInicio TIME,
+--                                      HoraFin TIME,
+--                                      PRIMARY KEY (IdEvento)
+--                                     );
+
+-- TRUNCATE Eventos;
+
+SELECT * FROM Eventos;
